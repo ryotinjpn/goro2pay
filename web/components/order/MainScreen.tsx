4fc4f96a2e433721a8ca99ecc1ac601795462396
ryotinjpn <ryotinjpn@gmail.com>
2026-05-27 12:18:51 +0900

    🎨 feat(web): MainScreen orchestrator を追加

---
+++ web/components/order/MainScreen.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useAtomValue, useSetAtom } from "jotai";
+
+import {
+  monthlyBudgetAtom,
+  screenStateAtom,
+} from "@/state/main";
+
+import { ScreenFrame } from "./ScreenFrame";
+import { BrandHeader } from "./BrandHeader";
+import { BalanceHero } from "./BalanceHero";
+import { GoroButton } from "./GoroButton";
+import { DeadVerdict } from "./DeadVerdict";
+import { IncreaseBudgetButton } from "./IncreaseBudgetButton";
+
+import styles from "./MainScreen.module.css";
+
+export function MainScreen() {
+  const screenState = useAtomValue(screenStateAtom);
+  const setMonthlyBudget = useSetAtom(monthlyBudgetAtom);
+
+  const handleIncrease = (nextBudget: number) => {
+    setMonthlyBudget(nextBudget);
+  };
+
+  return (
+    <ScreenFrame screenState={screenState} testid="main-screen">
+      <BrandHeader />
+      <BalanceHero />
+      <div className={styles.body}>
+        <GoroButton />
+      </div>
+      {screenState === "dead" && (
+        <>
+          <DeadVerdict />
+          <IncreaseBudgetButton onClick={handleIncrease} />
+        </>
+      )}
+    </ScreenFrame>
+  );
+}
